902ad9e7cd28e8e3b18e5ea5f9035e129e818e1b
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-01-19 09:31:40 -0800

    whole bunch of level stuff

---
+++ Assets/Eric/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	public float smoothSpd = 0.125f;
+	public float insert;
+	public Vector3 offset;
+
+	void FixedUpdate()
+	{
+		Vector3 desiredPosition = target.position + offset;
+		desiredPosition.y = insert;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpd * Time.deltaTime);
+		transform.position = smoothedPosition;
+	}
+}
