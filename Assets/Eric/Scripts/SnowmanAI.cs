33395e4695f35fa881ecb4237fe01773cb321e96
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-01-19 10:29:50 -0800

    snowman attacks

--- Assets/Eric/Scripts/SnowmanAI.cs
+++ Assets/Eric/Scripts/SnowmanAI.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SnowmanAI : MonoBehaviour
 {
+
+    public GameObject snowball;
+    public float cd = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,35 @@ public class SnowmanAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator cooldown()
+    {
+        yield return new WaitForSeconds(3);
+        cd = 0f;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            attack();
+            cd++;
+        }
+    }
+
+    private void attack()
+    {
+        if (cd == 0)
+        {
+            Debug.Log("attacking");
+            GameObject ball = Instantiate(snowball, this.transform.position, this.transform.rotation);
+            ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100f, 0f));
+        }
+        else
+        {
+            StartCoroutine(cooldown());
+        }
     }
 }
